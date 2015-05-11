379de6cce36942e3a7dfaa8259a664e899313f4f
John Grant <johnkgrant@gmail.com>
2015-05-11 00:42:18 -0400

    Add classes and structs

--- 05-Classes/Classes.playground/contents.swift
+++ 05-Classes/Classes.playground/contents.swift
@@ -1,28 +1,132 @@
 
 /*:
-# Basics
 
-Guiding Principles:
+# Classes
 
-* Safety
-* Functional
+Classes are similar to any other programming language. Big benefits from 
+Objective-C are:
+
+* No more header files (although, I like header files...)
+* No base class (but you still can inherit from `NSObject` if needed)
+
+Big thing to remember with classes, is that initializers must fully
+initialize all properties of a class.
 
 */
+
+//: No properties, no initializer required
+class Person {
+
+}
+let p = Person()
+
+
+//: Non-optional property must be set in the intializer
+class Dog {
+    let bread:String
+    init(bread:String){
+        self.bread = bread
+    }
+}
+let d = Dog(bread: "Pug")
+
+//: Can set default values on properties without requiring initializers
+class Car {
+    let color = "blue"
+}
+let c = Car()
+
+//: Can also set properties as optional
+class Exam {
+    var finalGrade:Int? = nil
+}
+let e = Exam()
+
+//: Inheritence allows for property overrides
+class Poodle:Dog {
+    init(){
+        super.init(bread:"Poodle")
+    }
+}
+let poodle = Poodle()
+
+//: Methods are just functions
+class Child {
+    func talk() {
+        println("I like candy")
+    }
+}
+let child = Child()
+child.talk()
+
+//: Overriding methods must use override keyword for compiler level safety
+class Toddler : Child {
+    override func talk() {
+        println("NO")
+    }
+}
+let toddler = Toddler()
+toddler.talk()
+
+//: Computed properties - must be `var` since value can change
+class Student {
+    let name = "Jane"
+    let school = "Western"
+    var description:String {
+        get {
+            return "\(name) goes to \(school)"
+        }
+    }
+}
+let student = Student()
+println(student.description)
+
+//: Built in property observers give access to `newValue` and `oldValue`
+class Beer {
+    var rating:Int{
+        willSet{
+            println("Setting rating to \(newValue)")
+        }
+        didSet{
+            println("Did set rating from \(oldValue)")
+        }
+    }
+
+    init() {
+        rating = 0
+    }
+}
+
+// Use cmd+opt+enter to see console output
+let beer = Beer()
+beer.rating = 5
+beer.rating = 10
+
 
 /*:
-### **Static Typing**
+### Mutability
 
-- Compiler is able to infer type of variables
+Classes are pass by reference. If declared as a `var`, you can 
+change the reference to a new class. If declared as `let`, the 
+reference cannot be changed, but the properties can be changed
+if they are declared as `var`
+
 */
 
-let numberAsInt = 3
+class Truck {
+    var weight = 1000
+    let VIN = 12345
+}
 
-let numberAsFloat = 3.0
+//: `let` cannot change the reference
+let t1 = Truck()
+//t1 = Truck() -> does not compile
 
-let result = numberAsInt + numberAsFloat
+//: `var` can change the reference
+var t2 = Truck()
+t2 = Truck()
 
-let string = "Hello"
-
-let array = [1,2,3,4,5]
-
-
+//: `let` can change var properties but not let 
+let t3 = Truck()
+t3.weight = 1235
+//t3.VIN = 1235632414 -> does not compile
---
+++ 05-Classes/Structs.playground/Contents.swift
@@ -0,0 +1,77 @@
+/*:
+# Structs
+
+Structs behave very similar to classes except for the following:
+
+* Structs **cannot** inherit from anything
+* Structs are **pass by value** rather than by reference
+* Structs have a "free" member initializer
+* Structs defined as `let` have properties that not mutable
+
+*/
+
+//: Free Initializer
+struct Size {
+    let width:Float
+    let height:Float
+}
+let s = Size(width: 100, height: 100)
+
+//: Can have computed properties
+struct AwesomeSize {
+    let width:Float
+    let height:Float
+    var area:Float{
+        get {
+            return width * height
+        }
+    }
+}
+let awesome = AwesomeSize(width: 100, height: 100)
+println(awesome.area)
+
+//: Can have methods
+struct Person {
+    let name:String
+
+    func speakName() {
+        println("My name is \(name)")
+    }
+}
+let p = Person(name: "john")
+p.speakName()
+
+
+/*:
+### Mutability
+*/
+
+struct Car {
+    let modelName:String
+    var color:String
+}
+
+//: let structs are not mutable even if var property
+let c = Car(modelName: "Ford", color: "Blue")
+//c.modelName = "Dodge" --> does not compile
+//c.color = "Red" --> does not compile
+
+var c2 = Car(modelName: "Honda", color: "Red")
+//c2.modelName = "Chevy" --> does not compile b/c it's a let
+c2.color = "Blue"
+
+//: If want to mutate var, must use `mutating` function
+struct BeerBottle {
+    var remaining:Int
+
+    mutating func drink() {
+        remaining-=100
+    }
+}
+
+var b = BeerBottle(remaining: 1000)
+b.drink()
+
+let bottle = BeerBottle(remaining: 1000)
+//bottle.drink() --> does not compile
+
